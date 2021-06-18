7e56d14c20f3bd4b573b2e8383cbc5bfa1543fe1
LadyNick <nnikki1013@gmail.com>
2021-06-17 17:38:29 -0700

    adding some small basics

--- practice.py
+++ practice.py
@@ -1,1 +1,9 @@
+'''Use 3 single quotes
+To make a multiline comment'''
 
+# this is a single line comment
+'''
+Use python filename.py to run a python file
+If you do python, it takes the next lines as python code,
+so use exit() to exit if you do that 
+'''
